5e6a087d654a4c010b970dcfed0ad50326a841eb
Stephen Oliver <steve@infincia.com>
2014-12-03 14:35:46 -0500

    Add constants file

---
+++ SafeDrive/SDConstants.h
@@ -0,0 +1,12 @@
+
+//  Copyright (c) 2014 Infincia LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString *const SDVolumeDidMountNotification;
+FOUNDATION_EXPORT NSString *const SDVolumeDidUnmountNotification;
+
+FOUNDATION_EXPORT NSString *const SDApplicationShouldOpenPreferencesWindow;
+FOUNDATION_EXPORT NSString *const SDApplicationShouldOpenAccountWindow;
+
---
+++ SafeDrive/SDConstants.m
@@ -0,0 +1,11 @@
+
+//  Copyright (c) 2014 Infincia LLC. All rights reserved.
+//
+
+#import "SDConstants.h"
+
+NSString *const SDVolumeDidMountNotification    = @"SDVolumeDidMountNotification";
+NSString *const SDVolumeDidUnmountNotification  = @"SDVolumeDidUnmountNotification";
+
+NSString *const SDApplicationShouldOpenPreferencesWindow    = @"SDApplicationShouldOpenPreferencesWindow";
+NSString *const SDApplicationShouldOpenAccountWindow        = @"SDApplicationShouldOpenAccountWindow";
