b7003cd565dbbbc1338cd1356f990fc6ed0fd69a
Stephen Oliver <steve@infincia.com>
2014-12-23 11:49:29 -0500

    Custom NSErrors and NSError domain for mounts and system API

--- SafeDrive/SDConstants.h
+++ SafeDrive/SDConstants.h
@@ -14,3 +14,22 @@ FOUNDATION_EXPORT NSString *const SDMountSubprocessDidTerminateNotification;
 FOUNDATION_EXPORT NSString *const SDApplicationShouldOpenPreferencesWindow;
 FOUNDATION_EXPORT NSString *const SDApplicationShouldOpenAccountWindow;
 
+#pragma mark - Errors
+
+FOUNDATION_EXPORT NSString *const SDErrorDomain;
+
+#pragma mark - Mount related errors
+
+FOUNDATION_EXPORT NSUInteger const SDMountErrorAuthorization;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorTimeout;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorMountFailed;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorUnmountFailed;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorAlreadyMounted;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorAskpassMissing;
+FOUNDATION_EXPORT NSUInteger const SDMountErrorUnknown;
+
+#pragma mark - System API related errors
+
+FOUNDATION_EXPORT NSUInteger const SDSystemErrorUnknown;
+FOUNDATION_EXPORT NSUInteger const SDSystemErrorAddLoginItemFailed;
+FOUNDATION_EXPORT NSUInteger const SDSystemErrorRemoveLoginItemFailed;
--- SafeDrive/SDConstants.m
+++ SafeDrive/SDConstants.m
@@ -11,3 +11,18 @@ NSString *const SDMountSubprocessDidTerminateNotification = @"SDMountSubprocessD
 
 NSString *const SDApplicationShouldOpenPreferencesWindow  = @"SDApplicationShouldOpenPreferencesWindow";
 NSString *const SDApplicationShouldOpenAccountWindow      = @"SDApplicationShouldOpenAccountWindow";
+
+NSString *const SDErrorDomain                             = @"com.safedrive";
+
+NSUInteger const SDMountErrorUnknown                      = 0x1001;
+NSUInteger const SDMountErrorAuthorization                = 0x1002;
+NSUInteger const SDMountErrorTimeout                      = 0x1003;
+NSUInteger const SDMountErrorMountFailed                  = 0x1004;
+NSUInteger const SDMountErrorUnmountFailed                = 0x1005;
+NSUInteger const SDMountErrorAlreadyMounted               = 0x1006;
+NSUInteger const SDMountErrorAskpassMissing               = 0x1007;
+
+
+NSUInteger const SDSystemErrorUnknown                     = 0x2001;
+NSUInteger const SDSystemErrorAddLoginItemFailed          = 0x2002;
+NSUInteger const SDSystemErrorRemoveLoginItemFailed       = 0x2003;
