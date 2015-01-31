d7323c53ede057ff9b07adb142789787b3e99c99
Stephen Oliver <steve@infincia.com>
2015-01-31 13:39:29 -0500

    Comment update

--- SafeDrive/AccountWindow/SDAccountWindow.m
+++ SafeDrive/AccountWindow/SDAccountWindow.m
@@ -110,19 +110,40 @@
 -(void)connectVolume {
     [self resetErrorDisplay];
     [self.spinner startAnimation:self];
-    /* 
-        This is somewhat fragile (the volume name must start with a slash) and 
-        in general it must be replaced before shipping because it isn't available 
-        in OS X 10.8 anyway
-        
-        A custom method for creating new NSURLs safely could be used, or just
-        skip NSURL entirely and use NSDictionary with constants for keys
-    */
 
+    /* 
+        Using NSURL here provides some validation of the parameters since we are
+        passing a standard RFC3986 URL string to SSHFS.
+
+        A custom NSURL category is being used because NSURLComponents isn't
+        available in OS X 10.8 (see below)
+
+    */
     NSURL *sshURL;
     #ifdef CUSTOM_NSURL
     sshURL = [NSURL SSHURLForAccount:self.emailField.stringValue password:self.passwordField.stringValue host:SDTestCredentialsHost port:@(SDTestCredentialsPort) path:self.volumeNameField.stringValue];
     #else
+    /*
+        This is the modern way to create an NSURL, but it is only available on
+        OS X 10.9+
+        
+        Things to keep in mind:
+
+        * The NSComponents URL property will just return nil if the parameters 
+          don't conform to RFC3986 (fragile if you don't check for that and handle it)
+        
+        * The user and password properties are only provided for compatibility
+          purposes and are technically deprecated in RFC3986. We're not giving
+          the password property directly to SSHFS (it's just used internally, 
+          an askpass helper binary is used for giving the password to SSHFS). We
+          ARE passing the user property though because that's how SSH logins work.
+        
+        * It isn't compatible with OS X 10.8, so we're not using it at the moment
+        
+        Whenever 10.8 support is dropped, THIS code should be used instead of
+        the custom NSURL category above
+
+    */
     NSURLComponents *urlComponents = [NSURLComponents new];
     urlComponents.user      = self.emailField.stringValue;
     urlComponents.password  = self.passwordField.stringValue;
