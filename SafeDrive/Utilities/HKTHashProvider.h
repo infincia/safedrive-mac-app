f14cf89978b1869022d4694241fa3612b5d956cb
Stephen Oliver <steve@infincia.com>
2015-11-13 14:26:09 -0500

    Add cryptographic hash wrappers

---
+++ SafeDrive/Utilities/HKTHashProvider.h
@@ -0,0 +1,18 @@
+
+//  Copyright (c) 2014 Infincia LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HKTHashProvider : NSObject
+
++ (NSString *)md5:(NSData *)data;
+
+
++ (NSString *)sha1:(NSData *)data;
++ (NSString *)sha224:(NSData *)data;
++ (NSString *)sha256:(NSData *)data;
++ (NSString *)sha384:(NSData *)data;
++ (NSString *)sha512:(NSData *)data;
+
+@end
