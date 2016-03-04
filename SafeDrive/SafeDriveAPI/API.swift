3203c94902895f63f57ca2f2c717ba697236594f
Stephen Oliver <steve@infincia.com>
2016-03-03 21:09:36 -0500

    Add Swift version of API wrapper

---
+++ SafeDrive/SafeDriveAPI/API.swift
@@ -0,0 +1,345 @@
+
+//  Copyright (c) 2014-2016 SafeDrive. All rights reserved.
+//
+
+
+import Foundation
+import Alamofire
+
+class API {
+    static let sharedAPI = API()
+
+    var reachabilityManager: AFNetworkReachabilityManager
+    var apiManager: AFHTTPSessionManager
+    var sharedSystemAPI = SDSystemAPI.sharedAPI()
+    var baseURL: NSURL?
+    
+    private var _session: String?
+    
+    var sessionToken: String? {
+        get {
+            if let session = self.sharedSystemAPI.retrieveCredentialsFromKeychainForService(SDSessionServiceName){
+                self.apiManager.requestSerializer.setValue(session["password"], forHTTPHeaderField: "SD-Auth-Token")
+                return session["password"]
+            }
+            return nil
+        }
+        set(newToken) {
+            _session = newToken
+            self.apiManager.requestSerializer.setValue(_session, forHTTPHeaderField: "SD-Auth-Token")
+        }
+    }
+    
+    init() {
+        self.reachabilityManager = AFNetworkReachabilityManager(forDomain: SDAPIDomainTesting)
+        self.reachabilityManager.setReachabilityStatusChangeBlock { (status: AFNetworkReachabilityStatus) -> Void in
+            switch status {
+            case .Unknown:
+                print("AFNetworkReachabilityStatusUnknown")
+            case .NotReachable:
+                print("AFNetworkReachabilityStatusNotReachable")
+            case .ReachableViaWWAN:
+                print("AFNetworkReachabilityStatusReachableViaWWAN")
+            case .ReachableViaWiFi:
+                print("AFNetworkReachabilityStatusReachableViaWiFi")
+            }
+        }
+        self.reachabilityManager.startMonitoring()
+        
+        self.baseURL = NSURL(string: "https://\(SDAPIDomainTesting)/api/1/")
+        self.apiManager = AFHTTPSessionManager(baseURL: self.baseURL)
+        self.apiManager.requestSerializer = AFJSONRequestSerializer()
+        // may not be necessary
+        self.apiManager.requestSerializer.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        self.apiManager.requestSerializer.setValue("application/json", forHTTPHeaderField: "Accept")
+        self.apiManager.responseSerializer = AFJSONResponseSerializer()
+
+        
+    }
+    
+    // MARK: Telemetry API
+    
+    func reportError(error: NSError, forUser user: String, withLog log: [String], completionQueue queue: dispatch_queue_t, success successBlock: SDSuccessBlock, failure failureBlock: SDFailureBlock) {
+        var postParameters = [NSObject : AnyObject]()
+        let os: String = "OS X \(self.sharedSystemAPI.currentOSVersion)"
+        postParameters["operatingSystem"] = os
+        let clientVersion: String = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+        postParameters["clientVersion"] = clientVersion
+        if !user.isEmpty {
+            let macAddress: String = self.sharedSystemAPI.en0MAC()
+            let machineIdConcatenation: String = macAddress.stringByAppendingString(user)
+            let identifier: String = HKTHashProvider.sha256(machineIdConcatenation.dataUsingEncoding(NSUTF8StringEncoding))
+            postParameters["uniqueClientId"] = identifier
+        }
+
+        postParameters["description"] = error.localizedDescription
+        
+        postParameters["context"] = error.domain
+        
+        postParameters["log"] = log.description
+        
+        let manager: AFHTTPSessionManager = AFHTTPSessionManager(baseURL: self.baseURL, sessionConfiguration: NSURLSessionConfiguration.ephemeralSessionConfiguration())
+        
+        manager.responseSerializer = AFJSONResponseSerializer()
+        manager.requestSerializer = AFJSONRequestSerializer()
+        manager.POST("error/log", parameters: postParameters, progress: nil, success: {(task: NSURLSessionTask, responseObject: AnyObject?) -> Void in
+            successBlock()
+        }, failure: {(task: NSURLSessionTask?, error: NSError) -> Void in
+            NSLog("Error: %@", error)
+            failureBlock(error)
+        })
+    }
+    
+    // MARK: Account API
+    
+    func registerMachineWithUser(user: String, password: String, success successBlock: SDAPIClientRegistrationSuccessBlock, failure failureBlock: SDFailureBlock) {
+        let languageCode: String = NSLocale.preferredLanguages()[0]
+        let os: String = "OS X \(self.sharedSystemAPI.currentOSVersion)"
+        let macAddress: String = self.sharedSystemAPI.en0MAC()
+        let machineIdConcatenation: String = macAddress.stringByAppendingString(user)
+        let identifier: String = HKTHashProvider.sha256(machineIdConcatenation.dataUsingEncoding(NSUTF8StringEncoding))
+        
+        let postParameters = ["email": user,
+            "password": password,
+            "operatingSystem": os,
+            "language": languageCode,
+            "uniqueClientId": identifier]
+        
+        Alamofire.request(.POST, "client/register", parameters: postParameters, encoding: .JSON)
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [String: String] else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    SDLog("Client registered: \(JSON)")
+                    guard let token = JSON["token"] else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Validation error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    self.sessionToken = token
+                    self.sharedSystemAPI.insertCredentialsInKeychainForService(SDSessionServiceName, account: user, password: token)
+                    successBlock(token)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+        
+    }
+
+    
+    func accountStatusForUser(user: String, success successBlock: SDAPIAccountStatusBlock, failure failureBlock: SDFailureBlock) {
+        Alamofire.request(.GET, "account/status", headers: ["SD-Auth-Token": self.sessionToken!])
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [String: String] else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    successBlock(JSON)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    func accountDetailsForUser(user: String, success successBlock: SDAPIAccountDetailsBlock, failure failureBlock: SDFailureBlock) {
+        Alamofire.request(.GET, "account/details", headers: ["SD-Auth-Token": self.sessionToken!])
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [String: String] else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    successBlock(JSON)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    // MARK: Sync folder handling
+    
+    func createSyncFolder(localFolder: NSURL, success successBlock: SDAPICreateSyncFolderSuccessBlock, failure failureBlock: SDFailureBlock) {
+        let postParameters = ["folderName": localFolder.lastPathComponent!, "folderPath": localFolder.path!]
+        
+        Alamofire.request(.POST, "folder", parameters: postParameters, encoding: .JSON, headers: ["SD-Auth-Token": self.sessionToken!])
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [String: AnyObject],
+                          let folderID: Int = JSON["id"] as? Int else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    successBlock(folderID)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    func readSyncFoldersWithSuccess(successBlock: SDAPIReadSyncFoldersSuccessBlock, failure failureBlock: SDFailureBlock) {
+        Alamofire.request(.GET, "folder", headers: ["SD-Auth-Token": self.sessionToken!])
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [[String: NSObject]] else {
+                            let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                            failureBlock(responseError)
+                            return
+                    }
+                    successBlock(JSON)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    func deleteSyncFolder(folderId: Int, success successBlock: SDAPIDeleteSyncFoldersSuccessBlock, failure failureBlock: SDFailureBlock) {
+        let folderIds = ["folderIds": folderId]
+        Alamofire.request(.DELETE, "folder", parameters: folderIds, encoding: .URLEncodedInURL, headers: ["SD-Auth-Token": self.sessionToken!])
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    successBlock()
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    // MARK: Unused
+    
+    func getHostFingerprintList(successBlock: SDAPIFingerprintListSuccessBlock, failure failureBlock: SDFailureBlock) {
+        Alamofire.request(.GET, "fingerprints")
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    guard let JSON = response.result.value as? [String: String] else {
+                        let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: "Internal error"])
+                        failureBlock(responseError)
+                        return
+                    }
+                    successBlock(JSON)
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+    
+    func apiStatus(successBlock: SDSuccessBlock, failure failureBlock: SDFailureBlock) {
+        Alamofire.request(.GET, "status")
+            .validate()
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                switch response.result {
+                case .Success:
+                    successBlock()
+                case .Failure(let error):
+                    print("Error: \(error)")
+                    guard let JSON = response.result.value as? [String: String], let message: String = JSON["message"] else {
+                        failureBlock(error)
+                        return
+                    }
+                    let responseError: NSError = NSError(domain: SDErrorAccountDomain, code: SDAPIError.Unknown.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
+                    failureBlock(responseError)
+                }
+        }
+    }
+}
