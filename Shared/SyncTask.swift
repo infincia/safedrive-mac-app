24a193984c3b0adc6ef0107497f4cf2b9f2284cc
Stephen Oliver <steve@infincia.com>
2016-03-09 17:36:05 -0500

    Update sync task model for tracking sync success and duration
    Also includes a "message" field which can be used to record error values
    for display at a later time or submission to telemetry API for debugging

--- Shared/SyncTask.swift
+++ Shared/SyncTask.swift
@@ -8,5 +8,30 @@ import Realm
 
 class SyncTask: Object {
     dynamic var syncFolder: SyncFolder?
+    
+    dynamic var uuid: String?
+    
+    // start of sync
     dynamic var syncDate: NSDate?
+    
+    // set to true only if sync finishes without error
+    dynamic var success: Bool = false
+    
+    // will be NSDate() - syncDate, calculated at time of success or failure
+    dynamic var duration: NSTimeInterval = 0
+    
+    // use for error messages if sync fails
+    dynamic var message: String?
+    
+    
+    convenience required init(syncFolder: SyncFolder, syncDate: NSDate, uuid: String) {
+        self.init()
+        self.syncFolder = syncFolder
+        self.syncDate = syncDate
+        self.uuid = uuid
+    }
+    
+    override static func primaryKey() -> String? {
+        return "uuid"
+    }
 }
