2d30f11c301d1951f41e066a2b2e79b840c62ca8
Stephen Oliver <steve@infincia.com>
2016-03-24 19:40:44 -0400

    Bump realm schema version

--- SafeDrive/AppDelegate.swift
+++ SafeDrive/AppDelegate.swift
@@ -143,9 +143,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, SDApplicationControlProtocol
                 path: dbURL.path,
                 // Set the new schema version. This must be greater than the previously used
                 // version (if you've never set a schema version before, the version is 0).
-                schemaVersion: 4,
+                schemaVersion: 5,
                 migrationBlock: { migration, oldSchemaVersion in
-                    if (oldSchemaVersion < 4) {
+                    if (oldSchemaVersion < 5) {
                         // No data changes, just new/removed properties
                     }
             })
