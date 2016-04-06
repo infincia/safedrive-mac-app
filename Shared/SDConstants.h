8d6afa2dbbc9da28baa7d2d199124a555f00bcc2
Stephen Oliver <steve@infincia.com>
2016-04-06 17:30:18 -0400

    Add nullability marking

--- Shared/SDConstants.h
+++ Shared/SDConstants.h
@@ -240,7 +240,7 @@ typedef void(^SDAPICreateSyncFolderSuccessBlock)(NSInteger folderID);
 typedef void(^SDAPIReadSyncFoldersSuccessBlock)(NSArray<NSDictionary<NSString *, NSObject *>*> * _Nonnull folders);
 typedef void(^SDAPIDeleteSyncFoldersSuccessBlock)();
 
-typedef void(^SDSyncProgressBlock)(double progress, NSString *bandwidth);
+typedef void(^SDSyncProgressBlock)(double progress, NSString * _Nonnull bandwidth);
 typedef void(^SDSyncResultBlock)(NSURL * _Nonnull syncURL, NSError * _Nullable syncError);
 
 #pragma mark - Global functions
