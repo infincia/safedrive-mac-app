49b3696abba75224bde2f0924429508c429511d1
Stephen Oliver <steve@infincia.com>
2017-03-18 23:26:57 -0400

    [App] remove test for MAC address and unique client IDs, replaced by SDK

--- SafeDriveTests/UnitTests.swift
+++ SafeDriveTests/UnitTests.swift
@@ -34,19 +34,4 @@ class UnitTests: XCTestCase {
         let testFolder = SyncFolder(name: "Home", path: "/Users/user/test", uniqueID: -1)
         XCTAssertFalse(SyncFolder.hasConflictingFolderRegistered("/Users/user/test2", syncFolders: [testFolder]))
     }
-    
-    func test_SDSystemAPI_en0MAC() {
-        let mac = SDSystemAPI.shared().en0MAC()
-        XCTAssertNotNil(mac)
-        NSLog("MAC en0: \(mac)")
-    }
-
-    func test_uniqueClientId() {
-            let macAddress: String = SDSystemAPI.shared().en0MAC()!
-            let machineIdConcatenation: String = macAddress + "stephen@safedrive.io"
-            let identifier: String = HKTHashProvider.sha256(machineIdConcatenation.data(using: String.Encoding.utf8))
-            print("ID: \(identifier)")
-            XCTAssert(identifier == "c19689af4055450b732a1e96400c6aa48d319e55239d66c84b2bdfcc48364faa")
-
-    }
 }
