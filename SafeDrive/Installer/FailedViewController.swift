dc8b77714b413452b8ee0c1447b7ef0c533a88f0
Stephen Oliver <steve@infincia.com>
2017-03-19 11:33:25 -0400

    [App] fix ridiculous NSTextView weak reference bug

--- SafeDrive/Installer/FailedViewController.swift
+++ SafeDrive/Installer/FailedViewController.swift
@@ -12,7 +12,14 @@ class FailedViewController: NSViewController {
     
     @IBOutlet var spinner: NSProgressIndicator!
     
-    @IBOutlet weak var errorMessage: NSTextView!
+    @IBOutlet var scrollView: NSScrollView!
+    
+    var errorMessage: NSTextView {
+        get {
+            return scrollView.contentView.documentView as! NSTextView
+        }
+    }
+
     @IBOutlet weak var panelMessage: NSTextField!
 
     @IBOutlet weak var reportButton: NSButton!
