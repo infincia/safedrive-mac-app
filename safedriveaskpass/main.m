2e7c7bbb0239c3debb4a20ac3f8944c3b69e00cc
Stephen Oliver <steve@infincia.com>
2017-04-01 20:25:45 -0400

    [Askpass] use the keychain to retrieve ssh credentials

--- safedriveaskpass/main.m
+++ safedriveaskpass/main.m
@@ -3,14 +3,34 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "sddk.h"
 
 static int SSHAskPassReturnValueSuccess = 0;
 static int SSHAskPassReturnValueFailure = 1;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *sshPassword = [[[NSProcessInfo processInfo] environment] objectForKey:@"SSH_PASSWORD"];
-        printf("%s", [sshPassword UTF8String]);
+        NSString *safedrive_currentuser_domain = [[[NSProcessInfo processInfo] environment] valueForKey:@"SAFEDRIVE_CURRENTUSER_DOMAIN"];
+        NSString *safedrive_account_domain = [[[NSProcessInfo processInfo] environment] valueForKey:@"SAFEDRIVE_ACCOUNT_DOMAIN"];
+        char * current_user = NULL;
+        SDDKError *current_user_error = NULL;
+        
+        if (0 != sddk_get_keychain_item("currentuser", [safedrive_currentuser_domain UTF8String], &current_user, &current_user_error)) {
+            sddk_free_error(&current_user_error);
+            return SSHAskPassReturnValueFailure;
+        }
+        
+        char * current_password = NULL;
+        SDDKError *current_password_error = NULL;
+        if (0 != sddk_get_keychain_item(current_user, [safedrive_account_domain UTF8String], &current_password, &current_password_error)) {
+            sddk_free_error(&current_password_error);
+            return SSHAskPassReturnValueFailure;
+        }
+        
+        printf("%s", current_password);
+
+        sddk_free_string(&current_user);
+        sddk_free_string(&current_password);
     }
     return SSHAskPassReturnValueSuccess;
 }
