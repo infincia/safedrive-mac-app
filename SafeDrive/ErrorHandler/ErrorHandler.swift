bb2417674838cdcafc3489c4d322ed76f314747c
Stephen Oliver <steve@infincia.com>
2017-04-01 20:21:14 -0400

    [App] remove comment

--- SafeDrive/ErrorHandler/ErrorHandler.swift
+++ SafeDrive/ErrorHandler/ErrorHandler.swift
@@ -46,7 +46,6 @@ func SDErrorHandlerSetUniqueClientId(_ uniqueClientId: String?) {
 
 func SDLog(_ line: String, _ arguments: CVarArg...) {
     return withVaList(arguments) {
-        // pass through to Crashlytics
         if !isProduction() {
             CLSNSLogv(line, $0)
         } else {
