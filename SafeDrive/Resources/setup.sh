295dc45fb7129f1a2e1a21058fcc2cd348cd6bca
Stephen Oliver <steve@infincia.com>
2017-04-12 00:26:10 -0400

    [App] fix setup script order

--- SafeDrive/Resources/setup.sh
+++ SafeDrive/Resources/setup.sh
@@ -3,7 +3,7 @@
 /bin/mkdir -p /usr/local
 /usr/sbin/chown root:wheel /usr/local
 
+/bin/mkdir -p /usr/local/bin
 /usr/sbin/chown ${USER} /usr/local/bin
 /usr/bin/chgrp admin /usr/local/bin
-/bin/mkdir -p /usr/local/bin
 /bin/chmod 755 /usr/local/bin
