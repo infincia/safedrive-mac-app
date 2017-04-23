26c50cac55319baeed0377bc759dfa835d6f553c
Stephen Oliver <steve@infincia.com>
2017-04-23 17:18:58 -0400

    [App] allow setup script to use a provided username for commands

--- SafeDrive/Resources/setup.sh
+++ SafeDrive/Resources/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+CURRENT_USER=$1
+
+
 /bin/mkdir -p /usr/local
 /usr/sbin/chown root:wheel /usr/local
 
 /bin/mkdir -p /usr/local/bin
-/usr/sbin/chown ${USER} /usr/local/bin
+/usr/sbin/chown ${CURRENT_USER} /usr/local/bin
 /usr/bin/chgrp admin /usr/local/bin
 /bin/chmod 755 /usr/local/bin
