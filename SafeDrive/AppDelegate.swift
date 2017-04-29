ea1466a6c64042756c1b9da79a1799a756f9e1ed
Stephen Oliver <steve@infincia.com>
2017-04-28 20:09:52 -0400

    [App] stop showing crash alerts for now

--- SafeDrive/AppDelegate.swift
+++ SafeDrive/AppDelegate.swift
@@ -375,8 +375,7 @@ extension AppDelegate: CrashlyticsDelegate {
         completionHandler(true)
         
         // show an alert telling the user a crash report was generated, allow them to opt out of seeing more alerts
-        CrashAlert.show()
-        
+        //CrashAlert.show()
     }
     
 }
