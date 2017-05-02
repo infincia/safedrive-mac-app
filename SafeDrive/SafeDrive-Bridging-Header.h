86d36dcef45b982b9fdbe03b11fa59e611d6ad95
Stephen Oliver <steve@infincia.com>
2017-05-01 22:47:58 -0400

    [All] remove the constants header from the pch and bridging headers

--- SafeDrive/SafeDrive-Bridging-Header.h
+++ SafeDrive/SafeDrive-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
-#import "SDConstants.h"
-
 #import "SDSystemAPI.h"
 
 @import DCOAboutWindow;
