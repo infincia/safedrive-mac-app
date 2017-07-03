7d392b0f01334ebc4981937d194e9a6f8cf30cf7
Stephen Oliver <steve@infincia.com>
2017-07-02 21:44:55 -0400

    [CI] remove installation scripts only needed by SDK

--- ci/install.sh
+++ ci/install.sh
@@ -6,34 +6,8 @@ set -ex
 
 . $(dirname $0)/utils.sh
 
-install_fuse() {
-    case "${TRAVIS_OS_NAME}" in
-        linux)
-            ;;
-        osx)
-            brew cask install osxfuse
-            ;;
-    esac
-}
-
-install_rustup() {
-    RUST_VER=$(<${RUST_VER_FILE})
-
-    echo "Using Rust ${RUST_VER}"
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=${RUST_VER}
-    rustup target add ${TARGET} || true
-    rustc -V
-    cargo -V
-    if [ ! -f ${HOME}/.cargo/bin/cheddar ]; then
-        cargo install moz-cheddar
-    else
-        echo "cheddar already installed, skipping"
-    fi
-}
-
 main() {
-    install_fuse
-    install_rustup
+
 }
 
 main
