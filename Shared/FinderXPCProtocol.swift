6f166d5a2cac5d32f545e981d054cc4fb70834c7
Stephen Oliver <steve@infincia.com>
2017-07-04 14:02:26 -0400

    [All] add finder xpc protocol

---
+++ Shared/FinderXPCProtocol.swift
@@ -0,0 +1,14 @@
+//  Copyright (c) 2014-2016 SafeDrive. All rights reserved.
+//
+
+import Foundation
+
+let kFinderXPCProtocolVersion: Int = 6
+
+@objc protocol FinderXPCProtocol {
+    func protocolVersion(_ replyBlock: @escaping (_ version: Int) -> Void)
+    func applicationDidConfigureClient(_ uniqueClientID: String)
+    func applicationDidConfigureUser(_ email: String)
+    func syncEvent(_ folderID: UInt64)
+    func mountStateChanged(_ mounted: Bool)
+}
