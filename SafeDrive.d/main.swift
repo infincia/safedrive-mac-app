0afd93c35a5a5f8627784965b38842ecfd3ec282
Stephen Oliver <steve@infincia.com>
2017-09-03 12:10:00 -0400

    [Daemon] use an autoreleasepool around the main runloop

--- SafeDrive.d/main.swift
+++ SafeDrive.d/main.swift
@@ -30,8 +30,9 @@ if #available(OSX 10.12, *) {
     NSLog("%@ listening", bundleId)
 }
 
-RunLoop.current.run()
-
+autoreleasepool {
+    RunLoop.current.run()
+}
 
 if #available(OSX 10.12, *) {
     os_log("%@ will exit", bundleId)
