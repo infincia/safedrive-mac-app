7a5a154f49ea62e4d636503513feb1c99b6c73ce
Stephen Oliver <steve@infincia.com>
2017-09-28 15:01:28 -0400

    [Project] rename sdfuse -> sdfs

---
+++ build_sdfs.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+DIST_DIR=${PWD}/dist
+BUILD_DIR=${PWD}/build
+
+mkdir -p ${DIST_DIR}
+mkdir -p ${BUILD_DIR}
+
+rm -rf ${BUILD_DIR}/sdfs*
+rm -rf ${DIST_DIR}/*
+
+./patch_sdfs.sh || ./patch_sdfs.sh || ./patch_sdfs.sh || ./patch_sdfs.sh
+
+pushd sdfs
+./build.sh -v 5 -t fsbundle -a build -- -s 10.13 -d 10.9 --kext=10.9 --kext="10.10->10.9" --kext=10.11 --kext="10.12->10.11" --kext="10.13->10.11"
+./build.sh -v 5 -t library -a build -- -s 10.13 -d 10.9
+popd
+
+ditto /tmp/sdfs/fsbundle/sdfs.fs ${DIST_DIR}/sdfs.bundle
+
+pushd ${DIST_DIR}/sdfs.bundle/Contents/Extensions
+
+popd
+
+codesign --verbose --sign "Developer ID Application: Stephen Oliver" ${DIST_DIR}/sdfs.bundle/Contents/Extensions/10.9/sdfs.kext
+codesign --verbose --sign "Developer ID Application: Stephen Oliver" ${DIST_DIR}/sdfs.bundle/Contents/Extensions/10.11/sdfs.kext
+codesign --verbose --sign "Developer ID Application: Stephen Oliver" ${DIST_DIR}/sdfs.bundle/Contents/Resources/mount_sdfs
+codesign --verbose --sign "Developer ID Application: Stephen Oliver" ${DIST_DIR}/sdfs.bundle/Contents/Resources/load_sdfs
+codesign --verbose --force --deep --sign "Developer ID Application: Stephen Oliver" ${DIST_DIR}/sdfs.bundle
+
+codesign --verbose --verify ${DIST_DIR}/sdfs.bundle/Contents/Extensions/10.9/sdfs.kext
+codesign --verbose --verify ${DIST_DIR}/sdfs.bundle/Contents/Extensions/10.11/sdfs.kext
+codesign --verbose --verify ${DIST_DIR}/sdfs.bundle/Contents/Resources/mount_sdfs
+codesign --verbose --verify ${DIST_DIR}/sdfs.bundle/Contents/Resources/load_sdfs
+codesign --verbose --verify ${DIST_DIR}/sdfs.bundle
+
+cp -a /tmp/sdfs/library/Source/lib/.libs/libsdfs.2.dylib ${DIST_DIR}/libsdfs.2.dylib
+install_name_tool -id "@rpath/libsdfs.2.dylib" ${DIST_DIR}/libsdfs.2.dylib
+
---
+++ patch_sdfs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+pushd sdfs
+
+files=("*.c" "*.h" "*.m" "*Info.plist" "*version.plist" "*.pbxproj" "*.sh" "*.am" "*.ac" "*.d" "*.in")
+for i in "${files[@]}"
+do
+echo "Files: $i"
+find . -name "$i" -type f
+find . -name "$i" -type f -exec sed -i '' s/osxfuse/sdfs/g {} +
+find . -name "$i" -type f -exec sed -i '' s/OSXFUSE/SDFS/g {} +
+find . -name "$i" -type f -exec sed -i '' s/com.github/io.safedrive/g {} +
+find . -name "$i" -type f -exec sed -i '' s/.filesystems.sdfs//g {} +
+done
+
+find . -type d -name '*osxfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/osxfuse/sdfs/g')' \;
+find . -name '*osxfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/osxfuse/sdfs/g')' \; || true
+
+popd
