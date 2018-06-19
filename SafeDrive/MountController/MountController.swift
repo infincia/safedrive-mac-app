53b4204748f688ee635ec0073656561ae649e34b
Stephen Oliver <steve@infincia.com>
2018-06-19 15:06:51 -0400

    [App] try to remove mount point on unmount
    May not always work, and may in some cases require using the
    privileged service if for some reason the permissions aren’t correct.
    
    We may also want to verify that this is ONLY going to remove an empty
    folder and will never remove *files*.

--- SafeDrive/MountController/MountController.swift
+++ SafeDrive/MountController/MountController.swift
@@ -641,6 +641,11 @@ extension MountController: SDVolumeEventProtocol {
         
         NSUserNotificationCenter.default.deliver(notification)
         
+        do {
+            try FileManager.default.removeItem(at: self.currentMountURL)
+        } catch let error {
+            SDLogError("MountController", "removing mountpoint failed: \(error.localizedDescription)")
+        }
     }
     
     func volumeSubprocessDidTerminate(notification: Notification) {
