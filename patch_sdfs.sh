3aa856b9fd352abf5bda1182864d4692ef42a78d
Stephen Oliver <steve@infincia.com>
2018-06-28 16:55:38 -0400

    [Build] remove unnecessary printed file paths

--- patch_sdfs.sh
+++ patch_sdfs.sh
@@ -8,7 +8,6 @@ files=("*.c" "*.h" "*.m" "*Info.plist" "*version.plist" "*.pbxproj" "*.sh" "*.am
 for i in "${files[@]}"
 do
 echo "Files: $i"
-find . -name "$i" -type f
 find . -name "$i" -type f -exec sed -i '' s/osxfuse/sdfs/g {} +
 find . -name "$i" -type f -exec sed -i '' s/OSXFUSE/SDFS/g {} +
 find . -name "$i" -type f -exec sed -i '' s/com.github/io.safedrive/g {} +
